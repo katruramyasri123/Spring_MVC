a48aa79328ba95c7726c2c5b210bcc0170b06e82
Ramya <Ramya@gmail>
2026-01-19 23:55:40 +0530

    first commit

---
+++ springmvcjava/src/main/java/com/twg/spring/springmvc/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package com.twg.spring.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@Configuration
+@ComponentScan("com.twg.spring.springmvc")
+@EnableWebMvc
+public class JavaConfig implements WebMvcConfigurer{
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+		
+	}
+	
+
+}
---
+++ springmvcjava/src/main/java/com/twg/spring/springmvc/controllers/HomeControllers.java
@@ -0,0 +1,14 @@
+package com.twg.spring.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeControllers {
+	@RequestMapping("home")
+	public ModelAndView getHome() {
+		ModelAndView model=new ModelAndView("home");
+		return model;
+	}
+}
